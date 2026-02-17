aa065312786607566b07c274bb45076cfe6c70b6
JuanBur10 <juanjosebur12@gmail.com>
2026-02-16 22:57:56 -0500

    Ejercicio 2 completado

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int width = 7;
+        int height = 5;
+
+        int area = width * height;
+        int perimeter = (2 * width) + (2 * height);
+
+        width += 3;
+        height -= 2;
+
+        area = width * height;
+        perimeter = (2 * width) + (2 * height);
+
+        Console.WriteLine("Width final: " + width);
+        Console.WriteLine("Height final: " + height);
+        Console.WriteLine("Area final: " + area);
+        Console.WriteLine("Perimeter final: " + perimeter);
+    }
+}
