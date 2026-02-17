7387254a57b7e5374079964967320d457bcba57f
JuanBur10 <juanjosebur12@gmail.com>
2026-02-16 22:46:00 -0500

    Ejercicio 1 completado y funcionando

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;
+        double height = 1.75;
+        bool is_student = true;
+        string name = "Ana";
+        char grade = 'A';
+
+        Console.WriteLine("La edad es: " + age);
+        Console.WriteLine("La altura es: " + height);
+        Console.WriteLine("¿Es estudiante?: " + is_student);
+        Console.WriteLine("El nombre es: " + name);
+        Console.WriteLine("La calificación es: " + grade);
+    }
+}
